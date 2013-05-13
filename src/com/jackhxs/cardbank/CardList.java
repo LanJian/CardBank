858252e079b47734f6b8f6d40d5fcb93e3a08f1e
Jack Huang <jackhxs@gmail.com>
2013-05-13 17:33:52 -0400

    minor changes

--- src/com/jackhxs/cardbank/CardList.java
+++ src/com/jackhxs/cardbank/CardList.java
@@ -1,8 +1,11 @@
 package com.jackhxs.cardbank;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CardList extends ListActivity {
   public void onCreate(Bundle icicle) {
@@ -13,5 +16,10 @@ public class CardList extends ListActivity {
     ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
         android.R.layout.simple_list_item_1, values);
     setListAdapter(adapter);
+  }
+
+  public void onListItemClick(ListView l, View v, int position, long id) {
+    Intent intent = new Intent(this, CardHolder.class);
+    startActivity(intent);
   }
 }
