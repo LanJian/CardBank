8babafb48932b62898db7db7e098024abb64e99e
Or Bar <orbar1@gmail.com>
2014-03-06 12:06:47 -0500

    minor change to template calculations
    Change-Id: If22b8fcfb8c8cde0758afdef354836849b47f325
    Signed-off-by: Or Bar <orbar1@gmail.com>

--- src/com/jackhxs/cardbank/BusinessCardFragment.java
+++ src/com/jackhxs/cardbank/BusinessCardFragment.java
@@ -6,6 +6,8 @@ import com.jackhxs.data.TemplateProperties.TextConfig;
 import com.jackhxs.remote.Constants;
 import com.squareup.picasso.Picasso;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,6 +17,7 @@ import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.RelativeLayout;
@@ -29,6 +32,8 @@ public class BusinessCardFragment extends Fragment {
 	private int padding = 0;
 	int width, height;
 
+	private Context mContext;
+	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		// TODO Auto-generated method stub
@@ -38,6 +43,7 @@ public class BusinessCardFragment extends Fragment {
 		outState.putParcelable("cardDetails", cardDetails);
 	}
     
+	@SuppressLint("NewApi")
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (savedInstanceState != null) {
@@ -47,71 +53,52 @@ public class BusinessCardFragment extends Fragment {
 		
 		ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.business_card_fragment, container, false);
         
-        RelativeLayout mBusinessCardLayout = (RelativeLayout) rootView.findViewById(R.id.business_card_layout);
-        ImageView background = (ImageView) rootView.findViewById(R.id.business_card_background);
+        final RelativeLayout mBusinessCardLayout = (RelativeLayout) rootView.findViewById(R.id.business_card_layout);
+        final ImageView background = (ImageView) rootView.findViewById(R.id.business_card_background);
         
-        getMeasurements();
-        
-        //Log.i(TAG, Constants.API_ADDRESS_V1 + template.imageUrl);
+        mContext = getActivity();
         
         Picasso.with(getActivity()).setDebugging(true);
         Picasso.with(getActivity()).load(Constants.API_ADDRESS_V1 + template.imageUrl).into(background);
         
-        addField(mBusinessCardLayout, template.properties.name, cardDetails.getFirstName() + " " + cardDetails.getLastName());
-        addField(mBusinessCardLayout, template.properties.phone, cardDetails.getPhone());
-        addField(mBusinessCardLayout, template.properties.email, cardDetails.getEmail());
-        addField(mBusinessCardLayout, template.properties.companyName, cardDetails.getCompanyName());
-        addField(mBusinessCardLayout, template.properties.address, cardDetails.getAddress());
-        addField(mBusinessCardLayout, template.properties.jobTitle, cardDetails.getJobTitle());
+        final ViewTreeObserver vto = background.getViewTreeObserver();
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @SuppressLint("NewApi")
+			public boolean onPreDraw() {
+            	background.getViewTreeObserver().removeOnPreDrawListener(this);
+                
+            	height = background.getMeasuredHeight();
+                width = background.getMeasuredWidth();
+                Log.i(TAG, "Height: " + height + " Width: " + width);
+                
+                
+                addField(mBusinessCardLayout, template.properties.name, cardDetails.getFirstName() + " " + cardDetails.getLastName());
+                addField(mBusinessCardLayout, template.properties.phone, cardDetails.getPhone());
+                addField(mBusinessCardLayout, template.properties.email, cardDetails.getEmail());
+                addField(mBusinessCardLayout, template.properties.companyName, cardDetails.getCompanyName());
+                addField(mBusinessCardLayout, template.properties.address, cardDetails.getAddress());
+                addField(mBusinessCardLayout, template.properties.jobTitle, cardDetails.getJobTitle());
+                
+                return true;
+            }
+        });
         
         return rootView;
     }
     
-	
-	private void getMeasurements() {
-		
-		float whScale = 0.541667f; // width to height ratio
-		
-		Display display = getActivity().getWindowManager().getDefaultDisplay();
-	    DisplayMetrics outMetrics = new DisplayMetrics ();
-	    display.getMetrics(outMetrics);
-
-	    float density  = getActivity().getResources().getDisplayMetrics().density;
-	    float dpWidth  = outMetrics.widthPixels / density;
-		
-	    dpWidth = dpWidth - 20; // there is a 10dp padding on either side
-	    float dpHeight = dpWidth * whScale;
-	    
-	    width = (int) (dpWidth * density);
-	    height = (int) (dpHeight * density);
-	}
-
-
 	private void addField(RelativeLayout mBusinessCardLayout, TextConfig fieldConfig, String value) {
 		
 		LayoutParams layoutParams = new LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-		
-        TextView textView = new TextView(getActivity());
+		TextView textView = new TextView(mContext);
         textView.setText(value);
         textView.setTextColor(Color.BLACK);
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
         
-        /*
-        Log.i("nameConfig", Boolean.toString(fieldConfig == null));
-        Log.i("nameConfig.left", Float.toString(fieldConfig.left));
-        Log.i("nameConfig.top", Float.toString(fieldConfig.top));
         
-        Log.i("layoutParams", Boolean.toString(layoutParams == null));
-        Log.i("layoutParams.leftMargin", Float.toString(layoutParams.leftMargin));
-        Log.i("layoutParams.topMargin", Float.toString(layoutParams.topMargin));
-        
-        Log.i("width", Float.toString(width));
-        Log.i("height", Float.toString(height));
-        */
-        layoutParams.leftMargin = (int) fieldConfig.left;// * width;
-        layoutParams.topMargin = (int) fieldConfig.top;// * height;
+        layoutParams.leftMargin = (int) (fieldConfig.left * width);
+        layoutParams.topMargin = (int) (fieldConfig.top * height);
         
         textView.setTextColor(Color.parseColor(fieldConfig.color));
         
@@ -119,8 +106,6 @@ public class BusinessCardFragment extends Fragment {
         mBusinessCardLayout.addView(textView);
 		
 	}
-
-	
 
 	public Template getTemplate() {
 		return template;
